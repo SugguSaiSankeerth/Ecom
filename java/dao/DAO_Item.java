979764df0b7ea81b6b1769f84eac1de0ce721158
PVK16 <Vamshi.Krishna@iiitb.org>
2019-02-11 22:28:19 +0530

    Adding DAO_Item.java

--- java/dao/DAO_Item.java
+++ java/dao/DAO_Item.java
@@ -1,5 +1,40 @@
 package dao;
+import java.sql.*;
+
+import dbcon.DatabaseConnection;
 
 public class DAO_Item {
+	String query = null;
+	ResultSet rs;
+	public boolean Add_Item(String description , float price, float discount, String name, String pic_location,
+			String category, String sub_category, String barcode, String dummy_1, String dummy_2, String dummy_3, String dummy_4) {
+		Connection conn = DatabaseConnection.getConnection();
+		PreparedStatement preparedStatement = null;
+		try {
+			query = "insert into item_table(descriptor,price,discount,name,pic_location,category,sub_category,barcode)"+" VALUES "+
+			"(?,?,?,?,?,?,?,?)";
+			preparedStatement = conn.prepareStatement(query);
+			preparedStatement.setString(1, description);
+			preparedStatement.setFloat(2, price);
+			preparedStatement.setFloat(3, discount);
+			preparedStatement.setString(4, name);
+			preparedStatement.setString(5, pic_location);
+			preparedStatement.setString(6, category);
+			preparedStatement.setString(7, sub_category);
+			preparedStatement.setString(8, barcode);
+			
+			int rs = preparedStatement.executeUpdate();
+			if(rs==0) {
+				return false;
+			}
+			else {
+				return true;
+			}
 
+		
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
 }
